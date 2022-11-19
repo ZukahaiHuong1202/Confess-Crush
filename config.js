7f2425dbfd2de67fb89857c58b828f7c37f809d9
ZukahaiHuong1202 <118616069+ZukahaiHuong1202@users.noreply.github.com>
2022-11-19 21:56:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Gửi tới người eo tương lai<333",
     introTitle: 'Xin Chào ĐẶNG QUANG HUY',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'iuiuiu',
+    introDesc: `Bánh chuối cần nướng
+    Đậu phụ cần tương
+    Dù em phi thường
+    Vẫn cần anh thương`,
+    btnIntro: 'nhấn vô đây đi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Cậu đồng ý làm người đàn ông của tớ?? ',
+    desc: 'Cậu đồng ý làm người đàn ông của tớ?? cậu yên tâm tớ sẽ bảo vệ cho cậu ><',
     btnYes: 'YES<33',
     btnNo: 'NO NEVER',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho người đàn ông của tui <3',
+    btnReply: 'Gửi cho Hưn',
     reply: 'Làm người đàn ông của cậu cũm được tôi đồng ý<33',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'vậy là từ nay cậu là người đàn ông của tớ goy đó nhé!!!',
