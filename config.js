413f58ff39d7dfd3f45e7cbdf193d48f3cfc7e3e
ZukahaiHuong1202 <118616069+ZukahaiHuong1202@users.noreply.github.com>
2022-11-20 22:05:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi tới người eo tương lai<333",
-    introTitle: 'Xin Chào Cậu Đặng QUANG HUY',
+    introTitle: 'Xin Chào Cậu NGUYỄN THỊ VÂN',
     introDesc: `Bánh chuối cần nướng
     Đậu phụ cần tương
     Dù em phi thường
-    Vẫn cần anh thương`,
+    Vẫn cần chị thương`,
     btnIntro: 'nhấn vô đây đi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Cậu đồng ý làm người đàn ông của tớ?? cậu yên tâm tớ sẽ bảo vệ cho cậu ><',
+    desc: 'Chị đồng ý làm người iu cụa em được hông?? ><',
     btnYes: 'YES<33',
     btnNo: 'NO NEVER',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho Hưn',
-    reply: 'Làm người đàn ông của cậu cũm được tôi đồng ý<33',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'vậy là từ nay cậu là người đàn ông của tớ goy đó nhé!!!',
+    reply: 'chị đồng ý',
+    mess: 'em biết mà 🥰.em iu chị gất nhìu 😘😘',
+    messDesc: 'vậy là từ nay chị là người yêu của em rồi đó nhé!!!',
     btnAccept: 'um oke!!',
     messLink: 'https://www.facebook.com/luu.nguyethuong.7?mibextid=ZbWKwL' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
